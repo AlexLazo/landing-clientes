b85549bcca81f557417454120f0b38e46aea2536
Brian Alexander Fuentes Lazo <62920279+AlexLazo@users.noreply.github.com>
2024-08-24 19:39:43 -0600

    Primeros cambios funcionales

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Contact = () => {
+  return (
+    <div>
+      <h1>Contact Us</h1>
+      <p>Get in touch with us through the following ways:</p>
+      <ul>
+        <li>Email: contact@example.com</li>
+        <li>Phone: +1-234-567-8900</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Contact;
---
+++ src/pages/Services.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Services = () => {
+  return (
+    <div>
+      <h1>Our Services</h1>
+      <p>We offer a range of services to help you achieve your goals:</p>
+      <ul>
+        <li>Service 1</li>
+        <li>Service 2</li>
+        <li>Service 3</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Services;
