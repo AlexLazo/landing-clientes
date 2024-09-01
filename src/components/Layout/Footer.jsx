3abab124147e063654e01e8cf3e1663d4d4abfc4
CesarNavarrete0 <cesarnavarrete01@gmail.com>
2024-08-31 19:45:40 -0600

    Implementacion del DARK MODE

--- src/components/Layout/Footer.jsx
+++ src/components/Layout/Footer.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import './Footer.css';
 
 const Footer = () => {
   return (
+    <div className='footer-group'>
     <footer>
       <p>&copy; {new Date().getFullYear()} Mr. Paquetes SV. All rights reserved.</p>
     </footer>
+    </div>
   );
 };
 
